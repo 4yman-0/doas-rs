5fe43f877f8f6fed876bcb59ba3b68e5f14542ee
4yman <170770027+4yman-0@users.noreply.github.com>
2024-10-28 12:41:08 +0100

    cargo update and fmt

--- src/cli.rs
+++ src/cli.rs
@@ -1,36 +1,29 @@
 use std::path::PathBuf;
 
-use clap::{Command, command, arg, value_parser};
+use clap::{arg, command, value_parser, Command};
 
 // Command Line Interface layout
 pub fn cmd() -> Command {
-    command!()
-        .subcommands([
-            // Define doas main command
-            Command::new("doas")
-                .about("Run a command as a different user, default is root")
-                .defer(|cmd| { 
-                    cmd.args(&[
-                        arg!(command: <COMMAND> ... "Command to run")
-                            .required(true)
-                            .value_parser(value_parser!(String)),
-
-                        arg!(user: -u -user <USER> "Arguments to pass to command")
-                            .required(false)
-                            .value_parser(value_parser!(String)),
-                            
-                        arg!(config: -C --config [config] "Path to config file")
-                            .required(false)
-                            .value_parser(value_parser!(PathBuf)),
-                    ])
-                }),
-
-            Command::new("doasedit")
-                .about("Edit a file as a different user"),
-
-            Command::new("vidoas")
-                .about("visual editor in doas"),
-        ])
+    command!().subcommands([
+        // Define doas main command
+        Command::new("doas")
+            .about("Run a command as a different user, default is root")
+            .defer(|cmd| {
+                cmd.args(&[
+                    arg!(command: <COMMAND> ... "Command to run")
+                        .required(true)
+                        .value_parser(value_parser!(String)),
+                    arg!(user: -u -user <USER> "Arguments to pass to command")
+                        .required(false)
+                        .value_parser(value_parser!(String)),
+                    arg!(config: -C --config [config] "Path to config file")
+                        .required(false)
+                        .value_parser(value_parser!(PathBuf)),
+                ])
+            }),
+        Command::new("doasedit").about("Edit a file as a different user"),
+        Command::new("vidoas").about("visual editor in doas"),
+    ])
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,16 @@
-use std::{process::Command, os::unix::process::CommandExt};
-use human_panic::setup_panic;
-use doas_rs::cli::cmd;
 use color_eyre::Result;
+use doas_rs::cli::cmd;
+use human_panic::setup_panic;
 use pam::PasswordConv;
+use std::{os::unix::process::CommandExt, process::Command};
 
 fn main() -> Result<()> {
-    setup_panic!(); // <-- initialize human-panic                                  
+    setup_panic!(); // <-- initialize human-panic
     let matches = cmd().get_matches();
 
     match matches.subcommand_name() {
         Some("doas") => {
-            let doas_matches = matches
-                .subcommand_matches("doas")
-                .unwrap();
+            let doas_matches = matches.subcommand_matches("doas").unwrap();
             let commands = doas_matches
                 .get_many::<String>("command")
                 .unwrap_or_default()
@@ -22,23 +20,20 @@ fn main() -> Result<()> {
             let user = doas_matches
                 .get_one::<String>("user")
                 .unwrap_or(&default_user);
-            
+
             // Authenticate session.
 
-            let execute_commands = Command::new(commands[0])
-                .args(&commands[1..])
-                .exec();
-
-        },
+            let execute_commands = Command::new(commands[0]).args(&commands[1..]).exec();
+        }
         Some("doasedit") => {
             todo!("doasedit");
-        },
+        }
         Some("vidoas") => {
             todo!("vidoas");
-        },
+        }
         _ => {
             cmd().print_help()?;
-        },
+        }
     }
     Ok(())
 }
