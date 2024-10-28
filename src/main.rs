9fac4446252e7047376b91310768d091931c51b9
4yman <170770027+4yman-0@users.noreply.github.com>
2024-10-28 13:37:13 +0100

    Use pam_client2 and users

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use color_eyre::Result;
 use doas_rs::cli::cmd;
 use human_panic::setup_panic;
-use pam::PasswordConv;
+use pam_client2::conv_cli::Conversation;
+use pam_client2::{Context, Flag};
 use std::{os::unix::process::CommandExt, process::Command};
+use users::get_user_by_name;
 
 fn main() -> Result<()> {
     setup_panic!(); // <-- initialize human-panic
@@ -20,10 +22,33 @@ fn main() -> Result<()> {
             let user = doas_matches
                 .get_one::<String>("user")
                 .unwrap_or(&default_user);
+            let uid = get_user_by_name(user).uid();
 
             // Authenticate session.
+            let mut context = Context::new(
+                "doas",              // Service name, decides which policy is used
+                None,                // Optional preset user name
+                Conversation::new(), // Handler for user interaction
+            )
+            .expect("Failed to initialize PAM context");
 
-            let execute_commands = Command::new(commands[0]).args(&commands[1..]).exec();
+            context.set_user_prompt(Some("Password:"));
+            context
+                .authenticate(Flag::NONE)
+                .expect("Authentication failed");
+            context
+                .acct_mgmt(Flag::NONE)
+                .expect("Authentication failed");
+            let mut session = context
+                .open_session(Flag::NONE)
+                .expect("Session opening failed");
+
+            Command::new(commands[0])
+                .args(&commands[1..])
+                .env_clear()
+                .envs(session.envlist().iter_tuples())
+                .uid(uid)
+                .exec();
         }
         Some("doasedit") => {
             todo!("doasedit");
